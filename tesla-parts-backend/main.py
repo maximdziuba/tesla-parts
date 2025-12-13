c64646963b7754e8325c1d8ed9aabccc015cad7d
Maksym Dziuba <dziuba200517@gmail.com>
2025-12-13 19:02:43 +0100

    CORS update

--- tesla-parts-backend/main.py
+++ tesla-parts-backend/main.py
@@ -24,7 +24,7 @@ origins = [
     "http://127.0.0.1:3001",
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "https://tesla-parts.vercel.app"
+    "https://tesla-parts.vercel.app",
     "https://tesla-parts-admin.vercel.app"
 ]
 
