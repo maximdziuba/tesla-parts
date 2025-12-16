abce8af7604ea0e84890e9cbe2780366c0376233
Maksym Dziuba <dziuba200517@gmail.com>
2025-12-16 11:52:36 +0100

    Added postgres

--- tesla-parts-backend/database.py
+++ tesla-parts-backend/database.py
@@ -2,18 +2,27 @@ from sqlmodel import SQLModel, create_engine, Session
 from sqlalchemy import text
 import os
 
-sqlite_file_name = "tesla_parts.db"
-base_dir = os.path.dirname(os.path.abspath(__file__))
-sqlite_path = os.path.join(base_dir, sqlite_file_name)
-sqlite_url = f"sqlite:///{sqlite_path}"
+DATABASE_URL = os.environ.get("DATABASE_URL")
 
-connect_args = {"check_same_thread": False}
-engine = create_engine(sqlite_url, connect_args=connect_args)
+if DATABASE_URL:
+    engine = create_engine(DATABASE_URL)
+else:
+    sqlite_file_name = "tesla_parts.db"
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    sqlite_path = os.path.join(base_dir, sqlite_file_name)
+    sqlite_url = f"sqlite:///{sqlite_path}"
+    connect_args = {"check_same_thread": False}
+    engine = create_engine(sqlite_url, connect_args=connect_args)
+
+
+def is_sqlite():
+    return engine.url.drivername == "sqlite"
 
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
-    _ensure_category_sort_order_column()
-    _ensure_product_cross_number_column()
+    if is_sqlite():
+        _ensure_category_sort_order_column()
+        _ensure_product_cross_number_column()
 
 def get_session():
     with Session(engine) as session:
